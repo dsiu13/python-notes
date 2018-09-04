57016c7d4f0349eb0a9b1566f261f93e17135d97
IamZombiez <dereksiu216@gmail.com>
2018-09-04 14:08:46 -0700

    update

--- crashCourse/crashCourse_ch11/employee_test.py
+++ crashCourse/crashCourse_ch11/employee_test.py
@@ -1,10 +1,17 @@
 import unittest
-import Employee from employee
-
+from employee import Employee
 
 class TestAnonSurvey(unittest.TestCase):
 
     def setUp(self):
-        self.new_employee = Employee("Bob", "Smith", "1000")
-        
+        self.new_employee = Employee("Bob", "Smith", 100000)
+
+    def test_give_raise(self):
+        self.new_employee.give_raise()
+        self.assertEqual(self.new_employee.annual_salary, 105000)
+
+    def test_give_custom_raise(self):
+        self.new_employee.give_raise(20000)
+        self.assertEqual(self.new_employee.annual_salary, 120000)
+
 unittest.main()
